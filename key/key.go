3e9e9ed9b4585679fdf9bb2c127e646ae4f6fe73
Franck JEANNIN <franck.jeannin@algosup.com>
2021-05-21 11:51:07 +0200

    Add keys

--- key/key.go
+++ key/key.go
@@ -8,6 +8,8 @@ var Right = ebiten.KeyRight
 var Left = ebiten.KeyLeft
 var Down = ebiten.KeyDown
 var Space = ebiten.KeySpace
+var Shift = ebiten.KeyShift
+var Control = ebiten.KeyControl
 
 func IsPressed(key ebiten.Key) bool {
 	return ebiten.IsKeyPressed(key)
